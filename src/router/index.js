f5382824458c4763d9991fa7959af9211943beae
ZyadElsaeed <devzyadelsaeed@gmail.com>
2026-09-14 16:07:32 +0300

    add write review page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventReviews from '../views/EventReviews.vue'
+import WriteReview from '../views/WriteReview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/event-reviews',
       name: 'EventReviews',
       component: EventReviews,
+    },
+    {
+      path: '/write-review',
+      name: 'WriteReview',
+      component: WriteReview,
     },
   ],
 })
